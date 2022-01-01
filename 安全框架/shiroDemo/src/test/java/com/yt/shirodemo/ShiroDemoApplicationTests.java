314069ac3c20088693a3daafe9f6a6be06fd15e3
应涛 <yt1577396227@163.com>
2022-01-01 18:08:08 +0800

    新增安全框架案例

---
+++ 安全框架/shiroDemo/src/test/java/com/yt/shirodemo/ShiroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yt.shirodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
